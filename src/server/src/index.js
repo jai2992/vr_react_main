6009722e3f11cfb8021c24dec0d5c24fe26c0246
Rajesh <rajesh.d2022ai-ml@sece.ac.in>
2024-03-11 10:27:35 +0530

    quiz page DEBUGGED !

--- src/server/src/index.js
+++ src/server/src/index.js
@@ -6,7 +6,7 @@ const authRoutes = require('./routes/authRoutes');
 const connectDB = require('./config/db');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
